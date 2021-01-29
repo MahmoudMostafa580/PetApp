05499e7451d35072d9a44c87bf0a197f28a7afb2
MahmoudMostafa580 <mahmoud.mostafa20112011@gmail.com>
2021-01-29 03:03:19 +0200

    intentLevel

--- app/src/main/java/com/example/shelter/Login.java
+++ app/src/main/java/com/example/shelter/Login.java
@@ -1,14 +1,29 @@
 package com.example.shelter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView registerText;
+    AppCompatButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerText=findViewById(R.id.register_text);
+        loginButton=findViewById(R.id.login_btn);
+        registerText.setOnClickListener(v -> startActivity(new Intent(Login.this,Register.class)));
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,MainActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shelter/Register.java
+++ app/src/main/java/com/example/shelter/Register.java
@@ -2,13 +2,24 @@ package com.example.shelter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    TextView signInText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        signInText=findViewById(R.id.signIn_text);
+        signInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this,Login.class));
+            }
+        });
     }
 }
